907f3981f885e4759b4286b9ee0b64b29dd0849e
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2019-12-30 02:58:04 -0600

    move to server

---
+++ build/precache-manifest.f1bacfc60b06e6716e0992c7004b6e54.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49440b510017aa105958f275e35b0a6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27263c8fbc0f9ef6cf61",
+    "url": "/static/css/main.007e9f71.chunk.css"
+  },
+  {
+    "revision": "da16a4f7bb0db4cf72c2",
+    "url": "/static/js/2.a56c83d2.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.a56c83d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27263c8fbc0f9ef6cf61",
+    "url": "/static/js/main.3d72a18e.chunk.js"
+  },
+  {
+    "revision": "d383dd1bc1e74c77566a",
+    "url": "/static/js/runtime-main.4398a03d.js"
+  },
+  {
+    "revision": "371c920ba10a46409479a49bd9cfc571",
+    "url": "/static/media/css3.371c920b.svg"
+  },
+  {
+    "revision": "23e2d9ce53c624499f8ad71bd24367cb",
+    "url": "/static/media/html5.23e2d9ce.svg"
+  },
+  {
+    "revision": "3f9ca62fc3b9eb791c709f59c0648d07",
+    "url": "/static/media/js.3f9ca62f.svg"
+  },
+  {
+    "revision": "566ee4a72dc2c68685e2bc0930708052",
+    "url": "/static/media/react.566ee4a7.svg"
+  }
+]);
