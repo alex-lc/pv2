1820b3e1e39d2acc8361a5d44a266a3688fb4564
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-02-07 17:29:18 -0600

    updated bw info

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -29,6 +29,13 @@ export default function Projects() {
             <h2>Previous Work and Projects</h2>
 
             <ProjectCard
+                title="PlantWise"
+                livePreview="https://plantwise.netlify.com"
+                github="https://github.com/Build-Week-WEB28-Water-My-Plants/frontend"
+                desc="In Build Week #3 of Lambda School, I served in the role of React II Developer and was in charge of the frontend. State management and CRUD operations interfacing with the backend API were my primary responsibilities, but I designed the UI as well using styled-components."
+                tech="Context API, React Hooks, React, styled-components, react-router-dom"
+            />
+            <ProjectCard
                 title="LifeGPA"
                 livePreview="https://lifegpa-lambda.netlify.com/"
                 github="https://github.com/BW-11-18-LIFE-GPA/__marketing"
